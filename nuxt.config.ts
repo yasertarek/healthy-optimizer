de5949c5f191cb7cd055974708c9c4c2dc84f6a4
Mohamed Ebada <111559889+mxebada@users.noreply.github.com>
2023-06-21 00:40:44 +0300

    last1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,5 +35,31 @@ export default defineNuxtConfig({
             })
           )
         }
+      },
+      compilerOptions: {
+        "types": [
+          "@nuxtjs/auth-next",
+        ]
+      },
+      auth: {
+        strategies: {
+          local: {
+            token: {
+              property: 'token',
+              global: true,
+              // required: true,
+              // type: 'Bearer'
+            },
+            user: {
+              property: 'user',
+              // autoFetch: true
+            },
+            endpoints: {
+              login: { url: '/api/auth/login', method: 'post' },
+              logout: { url: '/api/auth/logout', method: 'post' },
+              user: { url: '/api/auth/user', method: 'get' }
+            }
+          }
+        }
       }
 })
